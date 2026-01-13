dd4d69d8386f2c3156922b81e5d01e633c4abff3
Fairus In'am Pratama <fairusinam08@gmail.com>
2026-01-13 12:47:25 +0700

    fix: add storage link for prod + add favicon

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -45,6 +45,7 @@ class AdminPanelProvider extends PanelProvider
                 // Widgets\AccountWidget::class,
                 // Widgets\FilamentInfoWidget::class,
             ])
+            ->favicon(asset('favicon.png'))
             ->sidebarCollapsibleOnDesktop()
             ->middleware([
                 EncryptCookies::class,
