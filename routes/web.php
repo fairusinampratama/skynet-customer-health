b816f740f43b68e178e76a197d4e7a52bd6bf0e4
Fairus In'am Pratama <fairusinam08@gmail.com>
2026-01-17 09:55:17 +0700

    chore: add debug logging to reports download route

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,11 @@ Route::get('/tv/downtime', [TvController::class, 'downtime']);
 
 // Route to serve report files with specific filename headers
 Route::get('/reports/download/{filename}', function ($filename) {
+    \Illuminate\Support\Facades\Log::info("Download Reqq: $filename");
+    \Illuminate\Support\Facades\Log::info("Checking Path: reports/$filename on public disk");
+    
     if (!Storage::disk('public')->exists("reports/$filename")) {
+        \Illuminate\Support\Facades\Log::error("File NOT FOUND: reports/$filename");
         abort(404);
     }
     return Storage::disk('public')->download("reports/$filename");
