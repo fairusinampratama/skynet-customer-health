83c653b1d3ce8c5833213b44d5ae405f31c39143
Fairus In'am Pratama <fairusinam08@gmail.com>
2026-01-23 10:29:47 +0700

    fix(ui): optimize wallboard area names and add mock data seeders 🎨

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,6 +26,7 @@ class DatabaseSeeder extends Seeder
             XlsxImportSeeder::class,
             ServerImportSeeder::class,
             IsolationStateSeeder::class,
+            DummyAreaSeeder::class,
         ]);
     }
 }
