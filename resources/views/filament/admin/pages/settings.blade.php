965c254b511e40ed1ec9230071b25fbf0fbbbad9
Fairus In'am Pratama <fairusinam08@gmail.com>
2026-01-24 20:49:41 +0700

    style: remove duplicate manual action button from settings view

--- resources/views/filament/admin/pages/settings.blade.php
+++ resources/views/filament/admin/pages/settings.blade.php
@@ -1,25 +1,5 @@
 <x-filament-panels::page>
     <form wire:submit="save" class="space-y-6">
         {{ $this->form }}
-
-        <div class="border-t pt-6">
-            <h3 class="text-lg font-medium text-gray-900 dark:text-gray-100">Manual Actions</h3>
-            <div class="mt-4">
-                <x-filament::button 
-                    wire:click="sendReport" 
-                    wire:loading.attr="disabled"
-                    wire:target="sendReport"
-                    color="success"
-                    icon="heroicon-o-paper-airplane"
-                >
-                    <span wire:loading.remove wire:target="sendReport">
-                        Send Daily Report Now
-                    </span>
-                    <span wire:loading wire:target="sendReport">
-                        Sending... (Please wait)
-                    </span>
-                </x-filament::button>
-            </div>
-        </div>
     </form>
 </x-filament-panels::page>
