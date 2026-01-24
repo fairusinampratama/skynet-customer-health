7fe6a71c8982e4c52a3bf77648fb7ce3f4777dd8
Fairus In'am Pratama <fairusinam08@gmail.com>
2026-01-24 08:54:14 +0700

    refactor: remove redundant Send Daily Report action from Customers list (moved to Settings)

--- app/Filament/Admin/Resources/Customers/Pages/ListCustomers.php
+++ app/Filament/Admin/Resources/Customers/Pages/ListCustomers.php
@@ -14,23 +14,7 @@ class ListCustomers extends ListRecords
     {
         return [
             \Filament\Actions\CreateAction::make(),
-            \Filament\Actions\Action::make('sendDailyReport')
-                ->label('Send Daily Report')
-                ->color('success')
-                ->icon('heroicon-o-paper-airplane')
-                ->requiresConfirmation()
-                ->modalHeading('Send Daily Error Report')
-                ->modalDescription('Are you sure you want to generate and send the daily error report immediately? The title (Morning/Afternoon/Evening) will be based on the current time.')
-                ->modalSubmitActionLabel('Yes, Send it')
-                ->action(function () {
-                    \Illuminate\Support\Facades\Artisan::call('app:send-daily-error-report');
-                    
-                    \Filament\Notifications\Notification::make()
-                        ->title('Report Sent')
-                        ->body('The daily error report has been generated and sent to WhatsApp.')
-                        ->success()
-                        ->send();
-                }),
+
         ];
     }
 }
